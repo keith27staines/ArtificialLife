f2a84996c335a93243c29c6251b778a78dddf0ff
generic-keith <keith.staines.ext@bbc.co.uk>
2022-09-03 11:18:44 +0100

    Initial Commit

---
+++ ArtificialLife/ArtificialLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtificialLifeApp.swift
+//  ArtificialLife
+//
+//  Created by Keith Staines on 03/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ArtificialLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
